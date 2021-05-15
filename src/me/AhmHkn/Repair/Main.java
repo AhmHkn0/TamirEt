535e2557df4c2aea5e366d2d649d0c4ad5ae9455
AhmHkn0 <ahmhakan16@gmail.com>
2021-05-15 03:32:06 +0300

    English language support has been added.

--- src/me/AhmHkn/Repair/Main.java
+++ src/me/AhmHkn/Repair/Main.java
@@ -13,34 +13,40 @@ import org.bukkit.command.CommandSender;
 public final class Main extends JavaPlugin {
 
     public static Main plugin;
+    public static String locale;
     public static Economy econ = null;
 
     @Override
     public void onEnable() {
         plugin = this;
+        locale = Main.plugin.getConfig().getString("settings.locale");
+        if (!locale.equalsIgnoreCase("tr") || !locale.equalsIgnoreCase("en")) {
+            getServer().getConsoleSender().sendMessage(ChatColor.RED + "Unable locale. Only supports: 'tr' and 'en'");
+        }
         this.saveDefaultConfig();
         this.getCommand("tamir").setExecutor(new Repair());
-        getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "TamirEt eklentisi kuruldu.");
+        this.getCommand("repair").setExecutor(new Repair());
+        getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE + "TamirEt enabled.");
 
     }
     public void onDisable() {
         this.saveConfig();
-        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[TamirEt] Config kaydedildi.");
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[TamirEt] Config saved.");
     }
     public static void msg(Player p, String cfg) {
-        p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("mesajlar.prefix")) + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString(cfg)));
+        p.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("prefix")) + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString(cfg)));
     }
     public static void msgc(CommandSender s, String cfg) {
-        s.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("mesajlar.prefix")) + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString(cfg)));
+        s.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("prefix")) + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString(cfg)));
     }
-    public static Boolean giyilenZirhlar() {
-        return plugin.getConfig().getBoolean("ayarlar.giyilenzirhlar");
+    public static Boolean armors() {
+        return plugin.getConfig().getBoolean("settings.armors");
     }
-    public static Integer eltamircost() {
-        return plugin.getConfig().getInt("ayarlar.eltamircost");
+    public static Integer handrepaircost() {
+        return plugin.getConfig().getInt("settings.handrepaircost");
     }
-    public static Integer fulltamircost() {
-        return plugin.getConfig().getInt("ayarlar.fulltamircost");
+    public static Integer allrepaircost() {
+        return plugin.getConfig().getInt("settings.allrepaircost");
     }
 
     public boolean setupEconomy() {
--- src/me/AhmHkn/Repair/Repair.java
+++ src/me/AhmHkn/Repair/Repair.java
@@ -19,38 +19,46 @@ public class Repair implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
         RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
         econ = rsp.getProvider();
-        if (s.equalsIgnoreCase("tamir")) {
+        if (s.equalsIgnoreCase("tamir") || s.equalsIgnoreCase("tamiret:tamir") || s.equalsIgnoreCase("repair") || s.equalsIgnoreCase("tamiret:repair")) {
             if (sender instanceof Player) {
                 Player p = (Player) sender;
                 {
                     if (args.length == 0) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2TamirEt Plugini v" + Main.plugin.getDescription().getVersion() + " yapımcı: &6" + Main.plugin.getDescription().getAuthors()));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Komutlar:"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir et&7|&6hand&7|&6el"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir hepsi&7|&6all"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir reload &7&oSadece yetkililer"));
+                        if (Main.plugin.getConfig().getString("settings.locale").equalsIgnoreCase("tr")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2TamirEt Plugini v" + Main.plugin.getDescription().getVersion() + " yapımcı: &6" + Main.plugin.getDescription().getAuthors()));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Komutlar:"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir et&7|&6hand&7|&6el &a" + Main.plugin.getConfig().getInt("settings.handrepaircost") + " TL"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir hepsi&7|&6all &a" + Main.plugin.getConfig().getInt("settings.allrepaircost") + " TL"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir reload &7&oSadece yetkililer"));
+                        }else if (Main.plugin.getConfig().getString("settings.locale").equalsIgnoreCase("en")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2TamirEt Plugin v" + Main.plugin.getDescription().getVersion() + " Author: &6" + Main.plugin.getDescription().getAuthors()));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Commands:"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/repair et&7|&6hand&7|&6el &a$" + Main.plugin.getConfig().getInt("settings.handrepaircost")));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/repair hepsi&7|&6all &a$" + Main.plugin.getConfig().getInt("settings.allrepaircost")));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/repair reload &7&oOnly Operators"));
+                        }
                     } else if (args[0].equalsIgnoreCase("el") || args[0].equalsIgnoreCase("hand") || args[0].equalsIgnoreCase("et")){
                         if (p.getItemInHand().getType().getMaxDurability() != 0) {
                             if (p.getItemInHand().getDurability() > 0) {
-                                EconomyResponse r = econ.withdrawPlayer(p, Main.eltamircost());
+                                EconomyResponse r = econ.withdrawPlayer(p, Main.handrepaircost());
                                 if (r.transactionSuccess()) {
                                     p.getInventory().getItemInHand().setDurability((short) 0);
-                                    Main.msg(p, "mesajlar.tamiredildi");
+                                    Main.msg(p, Main.locale + ".messages.tamiredildi");
                                 } else {
-                                    Main.msg(p, "mesajlar.yetersizbakiye");
+                                    Main.msg(p, Main.locale + ".messages.yetersizbakiye");
                                 }
                             } else {
-                                Main.msg(p, "mesajlar.hasarsiz");
+                                Main.msg(p, Main.locale + ".messages.hasarsiz");
                             }
                         } else {
-                            Main.msg(p, "mesajlar.tamiredilemez");
+                            Main.msg(p, Main.locale + ".messages.tamiredilemez");
                             return true;
                         }
                 }else if (args[0].equalsIgnoreCase("hepsi") || args[0].equalsIgnoreCase("all")){
-                        EconomyResponse r = econ.withdrawPlayer(p, Main.fulltamircost());
+                        EconomyResponse r = econ.withdrawPlayer(p, Main.allrepaircost());
                         if (r.transactionSuccess()) {
-                            if (!Main.plugin.getConfig().getBoolean("ayarlar.giyilenzirhlar")) {
-                                Main.msg(p, "mesajlar.fulltamiredildi");
+                            if (!Main.plugin.getConfig().getBoolean("settings.giyilenzirhlar")) {
+                                Main.msg(p, Main.locale + ".messages.fulltamiredildi");
                                 for (int i = 0; i <= 35; i++) {
                                     try {
                                         if (p.getInventory().getItem(i).getType().getMaxDurability() != 0) {
@@ -60,8 +68,8 @@ public class Repair implements CommandExecutor {
 
                                     }
                                 }
-                            } else if (Main.plugin.getConfig().getBoolean("ayarlar.giyilenzirhlar")) {
-                                Main.msg(p, "mesajlar.fulltamiredildigiyilenzirhlar");
+                            } else if (Main.plugin.getConfig().getBoolean("settings.armors")) {
+                                Main.msg(p, Main.locale + ".messages.fulltamiredildigiyilenzirhlar");
                                 for (int i = 0; i <= 39; i++) {
                                     try {
                                         if (p.getInventory().getItem(i).getType().getMaxDurability() != 0) {
@@ -73,28 +81,48 @@ public class Repair implements CommandExecutor {
                                 }
                             }
                         }else{
-                            Main.msg(p, "mesajlar.yetersizbakiye");
+                            Main.msg(p, Main.locale + ".messages.yetersizbakiye");
                         }
                     }else if (args[0].equalsIgnoreCase("reload")) {
                         if (p.isOp()) {
                             Main.plugin.reloadConfig();
-                            Main.msgc(sender, "mesajlar.reload");
+                            Main.locale = Main.plugin.getConfig().getString("settings.locale");
+                            if (!Main.locale.equalsIgnoreCase("tr") && !Main.locale.equalsIgnoreCase("en")) {
+                                getServer().getConsoleSender().sendMessage(ChatColor.RED + "[TamirEt] Unable locale. Only supports: 'tr' and 'en'");
+                                sender.sendMessage((ChatColor.RED + "[TamirEt] Unable locale. Only supports: 'tr' and 'en'"));
+                                return false;
+                            }
+                            Main.msgc(sender, Main.locale + ".messages.reload");
                             getServer().getConsoleSender().sendMessage("[TamirEt] Eklenti ayarları yeniden yüklendi!");
                         }else if (!p.isOp()){
-                            Main.msg(p, "mesajlar.yetkisiz");
+                            Main.msg(p, Main.locale + ".messages.yetkisiz");
                         }
                     }else{
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2TamirEt Plugini v" + Main.plugin.getDescription().getVersion() + " yapımcı: &6" + Main.plugin.getDescription().getAuthors()));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Komutlar:"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir et&7|&6hand&7|&6el"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir hepsi&7|&6all"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir reload &7&oSadece yetkililer"));
+                        if (Main.plugin.getConfig().getString("settings.locale").equalsIgnoreCase("tr")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2TamirEt Plugini v" + Main.plugin.getDescription().getVersion() + " yapımcı: &6" + Main.plugin.getDescription().getAuthors()));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Komutlar:"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir et&7|&6hand&7|&6el &a" + Main.plugin.getConfig().getInt("settings.handrepaircost") + " TL"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir hepsi&7|&6all &a" + Main.plugin.getConfig().getInt("settings.allrepaircost") + " TL"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/tamir reload &7&oSadece yetkililer"));
+                        }else if (Main.plugin.getConfig().getString("settings.locale").equalsIgnoreCase("en")) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2TamirEt Plugin v" + Main.plugin.getDescription().getVersion() + " Author: &6" + Main.plugin.getDescription().getAuthors()));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Commands:"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/repair et&7|&6hand&7|&6el &a$" + Main.plugin.getConfig().getInt("settings.handrepaircost")));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/repair hepsi&7|&6all &a$" + Main.plugin.getConfig().getInt("settings.allrepaircost")));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/repair reload &7&oOnly Operators"));
+                        }
                     }
                 }
             }else if(args.length > 0){
                 if (args[0].equalsIgnoreCase("reload")){
                     Main.plugin.reloadConfig();
-                    Main.msgc(sender, "mesajlar.reload");
+                    Main.locale = Main.plugin.getConfig().getString("settings.locale");
+                    if (!Main.locale.equalsIgnoreCase("tr") && !Main.locale.equalsIgnoreCase("en")) {
+                        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[TamirEt] Unable locale. Only supports: 'tr' and 'en'");
+                        sender.sendMessage((ChatColor.RED + "[TamirEt] Unable locale. Only supports: 'tr' and 'en'"));
+                        return false;
+                    }
+                    Main.msgc(sender, Main.locale + ".messages.reload");
                     getServer().getConsoleSender().sendMessage("[TamirEt] Eklenti ayarları yeniden yüklendi!");
                 }else {
                     sender.sendMessage(ChatColor.RED + "[TamirEt] Bu komut konsoldan kullanilamaz!");
